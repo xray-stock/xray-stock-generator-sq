696c16eb8fca64585409aa6b96b7385bfe93d576
slicequeue <slicequeue@gmail.com>
2025-06-04 22:15:27 +0900

    chore: 스프링 프로젝트 생성 최초 커밋

---
+++ src/main/java/app/xray/stock/stock_generator/StockGeneratorApplication.java
@@ -0,0 +1,13 @@
+package app.xray.stock.stock_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockGeneratorApplication.class, args);
+	}
+
+}
